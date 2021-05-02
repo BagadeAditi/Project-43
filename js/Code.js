f2b69693a8062d4a0a90e3fb07678180d562f1de
BagadeAditi <79041998+BagadeAditi@users.noreply.github.com>
2021-05-02 15:22:25 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,40)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill(225);
+    textSize(15);
+    text("A T A D A S B A",700,140);
+    fill("lightblue");
+    text("Hint: Used for Multiplayer games.",700,170);
+
+    fill(225);
+    textSize(15);
+    text("C U T N I F O N",100,300);
+    fill("lightblue");
+    text("Hint: Call it to use it.",100,330);
+
+}
